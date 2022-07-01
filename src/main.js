13d9c7efccb79187612f7dd0c0dd2c500bab6d7c
liuweinb <2787601360@qq.com>
2022-07-01 23:05:05 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/index'
+
+
+import { Button } from 'vant';
+Vue.use(Button)
+
+
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    这是home页面
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'home',
+
 }
 </script>
